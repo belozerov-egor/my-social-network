f715ce643b1658ccabbd520589cfbc63f1dfb3c6
Egor <belaz.egor@gmail.com>
2023-07-10 17:33:45 +0300

    feat: add updateProfile form

--- src/features/Profile/ProfileEditForm/ProfileUpdateForm/ProfileUpdateForm.tsx
+++ src/features/Profile/ProfileEditForm/ProfileUpdateForm/ProfileUpdateForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {InjectedFormProps, reduxForm} from 'redux-form';
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 import {ProfileUserType} from "../../../../redux/store";
+import {Input, Textarea} from "../../../../common/components/FormControls/FormControls";
 
 type ProfileDataFormPropsType = {
     profile: ProfileUserType | null
@@ -9,7 +10,76 @@ type ProfileDataFormPropsType = {
 export const ProfileUpdateForm = (props: InjectedFormProps<ProfileUserType, ProfileDataFormPropsType> & ProfileDataFormPropsType) => {
     return (
         <form>
-
+            <div >
+                <h3>Полное имя:</h3>
+                <Field
+                    name={'fullName'}
+                    placeholder={'Полное имя'}
+                    component={Input}
+                    type={'text'}
+                    validate={[]}
+                />
+            </div>
+            <div >
+                <h3>Обо мне:</h3>
+                <Field
+                    name={'aboutMe'}
+                    placeholder={'Обо мне'}
+                    component={Input}
+                    type={'text'}
+                    validate={[]}
+                />
+            </div>
+            <div >
+                <h3>Обо мне:</h3>
+                <Field
+                    name={'aboutMe'}
+                    placeholder={'Обо мне'}
+                    component={Input}
+                    type={'text'}
+                    validate={[]}
+                />
+            </div>
+            <div >
+                <h3>Обо мне:</h3>
+                <Field
+                    name={'aboutMe'}
+                    placeholder={'Обо мне'}
+                    component={Input}
+                    type={'text'}
+                    validate={[]}
+                />
+            </div>
+            <div >
+                <h3 >В поисках работы:</h3>
+                <Field
+                    name={'lookingForAJob'}
+                    component={Input}
+                    text={'I am looking for a job'}
+                    type={'checkbox'}
+                    validate={[]}
+                    hello={'hello text'}
+                />
+                <Field
+                    name={'lookingForAJobDescription'}
+                    placeholder={'My professional skills'}
+                    component={Textarea}
+                    type={'text'}
+                    validate={[]}
+                />
+            </div>
+            {props.profile && Object.keys(props.profile.contacts).map((key, index) => {
+                return <div key={index} >
+                    <h3 >{key}:</h3>
+                    <Field
+                        name={'contacts.' + key}
+                        placeholder={key}
+                        component={Input}
+                        type={'text'}
+                        validate={[]}
+                    />
+                </div>
+            })}
         </form>
     );
 };
